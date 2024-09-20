b9299e071a2fb45e75aec8bb76bd5811444f2212
Zalán Tóth <zalant.tsc@gmail.com>
2024-09-20 15:47:58 +0100

    Completed Tasky Lab 1

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" />
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"></Task>
+      <Task title="Tidy" deadline="Today" description="This is a creative describe text :-)" />
     </div>
   );
 }
