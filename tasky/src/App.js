d8ce2e783ca4acbf49c31f944f28d633f0b2f46b
Zalán Tóth <zalant.tsc@gmail.com>
2024-10-03 20:58:15 +0100

    Added keys to tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,9 +6,9 @@ import Task from './components/Task';
 function App() {
   const [ taskState, setTaskState ] = useState({
       tasks: [
-          { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-          { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-          { title: "Tidy up", deadline: "Today" }
+          { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+          { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+          { id: 3, title: "Tidy up", deadline: "Today" }
       ]
   });
   return (
@@ -19,6 +19,7 @@ function App() {
                   title={task.title}
                   description={task.description}
                   deadline={task.deadline}
+                  key={task.id}
               />
           ))}
       </div>
